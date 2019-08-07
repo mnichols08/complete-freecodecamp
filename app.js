494659e8e3ccc7826c56389de8d57707684d10c3
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2019-08-07 06:23:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-document.body.append(document.createElement('h1'));
-document.querySelector('h1').innerText = `Calculate Time to Complete Free Code Camp`;
-document.body.append(document.createElement('form'));
-document.querySelector('form').innerHTML = `
+(function(){
+
+function calcTime(courseLength, hoursPerDay, daysPerWeek, interviewPrep){
+	
+	this.time = (courseLength / hoursPerDay) + ((courseLength / hoursPerDay) / daysPerWeek);
+	this.date = new Date(new Date().setDate(new Date().getDate() + this.time));
+	document.querySelector('h1').innerText = `You should finish by ${this.date.toString().slice(0,15)}`;
+	console.log({'actual days to complete': this.time, 'hours of study': courseLength, 'days per week': daysPerWeek, 'hours for interview prep': interviewPrep});
+
+};
+
+const getTime = e => {
+e.preventDefault();
+    let courses = document.querySelector('select').value,
+        hours = courses * 300,
+        htCommit = document.querySelector('form input[name="hoursPerDay"]').value,
+        dtCommit = document.querySelector('form input[name="daysPerWeek"]').value,
+        interview = document.querySelector('form input[name="interviewPrep"]').value;
+        calcTime(hours, htCommit, dtCommit, interview);
+}
+
+const body = ele => document.body.append(ele);
+
+const h1 = document.createElement('h1');
+const p = document.createElement('p');
+const form = document.createElement('form');
+
+body(h1);
+body(p);
+body(form);
+
+h1.innerText = `Calculate Time to Complete Free Code Camp`;
+form.innerHTML = `
   <h3>Number of Courses To Complete</h3>
   <select>
   <option value="6">6</option>
@@ -13,29 +42,19 @@ document.querySelector('form').innerHTML = `
   </select>
   
   <h3>How many hours can you commit per day?</h3>
-  <input type="range" name="hoursPerDay" min="1" max="24" value="12">
+  <input type="range" name="hoursPerDay" min="1" max="24" value="18">
 
   <h3>How many days can you commit per week?</h3>
   <input type="range" name="daysPerWeek" min="1" max="7" value="7">
 
   <h3>How many hours of interviews practice do you want to calculate?</h3>
-  <input type="range" name="interviewPrep" min="0" max="2500" value="1">
+  <input type="range" name="interviewPrep" min="0" max="2500" value="0">
 
   <button type="submit">Calculate</button>
 `;
-document.querySelector('form button').addEventListener('click', e => {
-e.preventDefault();
-    let courses = document.querySelector('select').value,
-        hours = courses * 300,
-        htCommit = document.querySelector('form input[name="hoursPerDay"]').value,
-        dtCommit = document.querySelector('form input[name="daysPerWeek"]').value,
-        interview = document.querySelector('form input[name="interviewPrep"]').value;
-        calcTime(hours, htCommit, dtCommit, interview);
-});
-function calcTime(courseLength, hoursPerDay, daysPerWeek, interviewPrep){
-	
-	this.time = (courseLength / hoursPerDay) + ((courseLength / hoursPerDay) / daysPerWeek);
-	this.date = new Date(new Date().setDate(new Date().getDate() + this.time));
-	document.querySelector('h1').innerText = `You should finish by ${this.date}`;
-	console.log({'actual days to complete': this.time, 'hours of study': courseLength, 'days per week': daysPerWeek, 'hours for interview prep': interviewPrep});
-};
+
+document.querySelector('form button').addEventListener('click', getTime);
+document.querySelectorAll('form input').forEach(e => e.addEventListener('input', getTime));
+
+})()
+undefined
