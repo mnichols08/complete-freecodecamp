dcd983b2a2a51786da8b62194874179b58d979ce
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2019-08-07 06:32:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 (function(){
 
 function calcTime(courseLength, hoursPerDay, daysPerWeek, interviewPrep){
-	
-	this.time = (courseLength / hoursPerDay) + ((courseLength / hoursPerDay) / daysPerWeek);
+	const allHours = parseInt(interviewPrep + courseLength);
+	this.time = (allHours / hoursPerDay) + ((allHours / hoursPerDay) / daysPerWeek);
 	this.date = new Date(new Date().setDate(new Date().getDate() + this.time));
-	document.querySelector('h1').innerText = `You should finish by ${this.date.toString().slice(0,15)}`;
-	console.log({'actual days to complete': this.time, 'hours of study': courseLength, 'days per week': daysPerWeek, 'hours for interview prep': interviewPrep});
+	document.querySelector('h1').innerText = `You should finish by ${this.date}`;
+// 	console.log({'actual days to complete': this.time, 'hours of study': courseLength, 'days per week': daysPerWeek, 'hours for interview prep': interviewPrep});
 
 };
 
@@ -48,7 +48,7 @@ form.innerHTML = `
   <input type="range" name="daysPerWeek" min="1" max="7" value="7">
 
   <h3>How many hours of interviews practice do you want to calculate?</h3>
-  <input type="range" name="interviewPrep" min="0" max="2500" value="0">
+  <input type="range" name="interviewPrep" min="0" max="100" value="0">
 
   <button type="submit">Calculate</button>
 `;
@@ -57,4 +57,3 @@ document.querySelector('form button').addEventListener('click', getTime);
 document.querySelectorAll('form input').forEach(e => e.addEventListener('input', getTime));
 
 })()
-undefined
