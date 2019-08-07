2f5d21a370b2ae0fbe0e9e055d672e2e8dd0fdf2
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2019-08-07 15:35:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const calcTime = (courseLength, hoursPerDay, daysPerWeek, interviewPrep) => {
 	h1.innerText = `You should finish by ${calcDate.toString().slice(0,15)}`;
 	;
 	p.innerText = `
-Each unit in freeCodeCamp is approximately 300 hours. 
-If you signed up for ${courseLength / 300 > 1 ? `${courseLength / 300} units` : `${courseLength / 300} unit`} and you devote at least 
-${interviewPrep > 1 ? `${interviewPrep} hours` : `${interviewPrep} hour`} to your interview prep, it will take you a total of ${calcHours} hours. 
+Each unit in freeCodeCamp is approximately 300 hours.
+If you signed up for ${courseLength / 300 > 1 ? `${courseLength / 300} units` : `${courseLength / 300} unit`} and you devote at least
+${interviewPrep > 1 ? `${interviewPrep} hours` : `${interviewPrep} hour`} to your interview prep, it will take you a total of ${calcHours} hours.
 Assuming that you were to spend ${hoursPerDay > 1 ? `${hoursPerDay} hours` : `${hoursPerDay} hour`} per day for ${daysPerWeek > 1 ? `${daysPerWeek} days` : `${daysPerWeek} day`} each week,
-You would be finished in as soon as ${calcTime > 1 ? `${calcTime.toFixed()} days` : `${calcTime.toFixed()} day`}! 
+You would be finished in as soon as ${calcTime > 1 ? `${calcTime.toFixed()} days` : `${calcTime.toFixed()} day`}!
 Guess what?! That is ${calcTime > 365 ? `only ${(calcTime / 365).toFixed()} years from now!` : `less than ${(calcTime / 365 * 12).toFixed()} months from now!`}
 `;
 };
@@ -29,6 +29,32 @@ e.preventDefault();
 	document.querySelectorAll('span value')[1].innerText = `${dtCommit > 1 ? `${dtCommit} hours` : `${dtCommit} hour`}.`;
 	document.querySelectorAll('span value')[2].innerText = `${interview > 1 ? `${interview} hours` : `${interview} hour`}.`;
 }
+
+document.head.append(document.createElement('style'));
+document.querySelector('style').innerHTML = `
+body {
+    font-family: 'Open Sans', sans-serif;
+    background: linear-gradient(45deg, hsl(0, 5%, 10%), hsl(0, 13%, 11%));
+    color: #eee;
+    width: 50%;
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+div {
+    position:  absolute;
+    top: 50%;
+    left: 50%;
+    transform:  translate(-50%,-50%);
+    padding: 2.5%;
+    margin: 2.5%;
+    background: linear-gradient(-45deg, hsla(280, 69%, 14%, 0.62), hsla(150, 69%, 8%, 0.75));
+    border-radius: 6pt;
+    box-shadow: 0 0 5pt 0 hsla(0, 0%, 0%, 0.57);
+};
+
+`;
 
 const render = ele => div.prepend(ele);
 
